7262da10c545e4314aa690b409f9412c73823653
SebiB2004 <dariusbabut10@gmail.com>
2024-09-25 13:46:26 +0300

    add Main.java

---
+++ testProject/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Salut!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
